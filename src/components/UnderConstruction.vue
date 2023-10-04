d3ba93891fb83e1ace9822503a678b38c1fe72e2
Andreas Richter <a.richter@dotsource.de>
2023-10-04 10:36:32 +0200

    Added UnderConstruction.vue

---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="under-construction">
+    <h1>Under Construction</h1>
+    <p>This page is currently under construction. Please check back later!</p>
+    <img src="construction.gif" alt="Under Construction" />
+  </div>
+</template>
+
+<style scoped>
+.under-construction {
+  text-align: center;
+  margin: 100px auto;
+}
+
+h1 {
+  color: #333;
+}
+
+p {
+  color: #666;
+  margin-bottom: 20px;
+}
+
+img {
+  max-width: 100%;
+}
+</style>
