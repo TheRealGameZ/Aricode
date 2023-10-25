311851e59a46d0575ce2c574acbc608be2e3f619
TheRealGameZ <GameZ22@gmx.de>
2023-10-25 18:10:18 +0200

    Golol

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,63 @@
 <template>
-  <navigation-bar></navigation-bar>
+  <header>
+    <div class="header-container">
+      <h1><span class="textPart1">Welcome</span> <span class="textPart2">to my</span><br><span class="textPart3">Site!</span></h1>
+    </div>
+  </header>
 </template>
 
 
 <script setup>
-  import NavigationBar from "@/components/NavigationBar.vue";
 </script>
+
+<style>
+  header {
+    background-image: url(assets/background.svg);
+    min-height: 100vh;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    font-family: 'Work Sans', sans-serif;
+    text-transform: uppercase;
+  }
+
+  .header-container{
+    padding: 30% 0;
+    text-align: center;
+  }
+
+  .textPart1{
+    opacity: 0;
+    animation: myAnim 2.5s ease-in-out 0s 1 normal forwards;
+  }
+
+  .textPart2{
+    opacity: 0;
+    animation: myAnim 2.5s ease-in-out 0s 1 normal forwards;
+    animation-delay: 0.6s;
+  }
+
+  .textPart3{
+    opacity: 0;
+    animation: myAnim 2.5s ease-in-out 0s 1 normal forwards;
+    animation-delay: 1.2s;
+  }
+
+  .textPart4{
+    opacity: 0;
+    animation: myAnim 2.5s ease-in-out 0s 1 normal forwards;
+    animation-delay: 1.2s;
+  }
+
+  @keyframes myAnim {
+	0% {
+		opacity: 0;
+	}
+
+	100% {
+		opacity: 1;
+	}
+}
+
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="content">
-    <under-construction></under-construction>
   </div>
 </template>
 
 <script>
-import UnderConstruction from "@/components/UnderConstruction.vue";
 
 export default {
   name: 'HomeView',
   components: {
-    UnderConstruction
+
   }
 }
 </script>
